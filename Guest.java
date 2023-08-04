e55ebb586e7c7e39135ce6b3ea3f559a9d9e5ba7
Stephen ombuya <129269603+STEPHEN07383@users.noreply.github.com>
2023-08-04 14:56:58 +0300

    Add files via upload

---
+++ Guest.java
@@ -0,0 +1,30 @@
+package hotelmanagementsystem;
+
+public class Guest {
+
+	    private String name;
+	    private String email;
+
+	    public Guest(String name, String email) {
+	        this.name = name;
+	        this.email = email;
+	    }
+
+	    // Getters and setters
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+	}
