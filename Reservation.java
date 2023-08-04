b44596e0f620993a4cf31a7c2157dc37ae3dfd21
Stephen ombuya <129269603+STEPHEN07383@users.noreply.github.com>
2023-08-04 14:58:23 +0300

    Add files via upload

---
+++ Reservation.java
@@ -0,0 +1,81 @@
+package hotelmanagementsystem;
+
+import java.util.Date;
+
+public class Reservation {
+    private int reservationId;
+    private Guest guest;
+    private Room room;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private boolean isCheckedIn;
+    private boolean isCheckedOut;
+
+    public Reservation(int reservationId, Guest guest, Room room, Date checkInDate, Date checkOutDate) {
+        this.reservationId = reservationId;
+        this.guest = guest;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.isCheckedIn = false;
+        this.isCheckedOut = false;
+    }
+
+    // Getters and setters
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public boolean isCheckedIn() {
+        return isCheckedIn;
+    }
+
+    public void setCheckedIn(boolean checkedIn) {
+        isCheckedIn = checkedIn;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+}
